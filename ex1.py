9effc881a30044b56cda1fa16e2654b528a0ed26
Cat Chen <cathsfz@gmail.com>
2011-06-29 21:28:46 +0800

    finished exercise 3

--- ex1.py
+++ ex1.py
@@ -1,7 +1,9 @@
-print "Hello World!"
-print "Hello Again"
-print "I like typing this."
-print "This is fun."
-print "Yay! Printing."
-print "I'd much rather you 'not'."
-print 'I "said" do not touch this.'
+print("Hello World!")
+print("Hello Again")
+print("I like typing this.")
+print("This is fun.")
+print("Yay! Printing.")
+print("I'd much rather you 'not'.")
+print('I "said" do not touch this.')
+print('''Print one line.
+Print another line.''')
--- progress.md
+++ progress.md
@@ -5,4 +5,6 @@ This is a file used to track my progress.
 ## 2011-06-29
 
 1. Skip exercise 0 because I'll just use TextMate
-2. 
+2. Finish exercise 1 and also make it Python3 compatible
+3. Finish exercise 2
+4. Finish exercise 3 and found that [division operator](http://www.python.org/dev/peps/pep-0238/) has different meanings in Python2 and Python3
