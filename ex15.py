00c0ca98a2db77b1d29b61f56e4d4ba37c32dc75
Cat Chen <cathsfz@gmail.com>
2011-07-04 09:47:04 +0800

    finised exercise 15

--- ex15.py
+++ ex15.py
@@ -7,9 +7,13 @@ txt = open(filename)
 print("Here's your file %r:" % filename)
 print(txt.read())
 
+txt.close();
+
 print("I'll also ask you to type it again:")
 file_again = input("> ")
 
 txt_again = open(file_again)
 
 print(txt_again.read())
+
+txt_again.close()
